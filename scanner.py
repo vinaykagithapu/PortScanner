a890a258d0047bd0b85ce282ae33188248969454
vinaykagithapu <33367418+vinaykagithapu@users.noreply.github.com>
2020-07-11 10:08:17 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,22 +4,23 @@ import sys
 import socket
 from datetime import datetime
 
-#Defining Target
-if len(sys.argv)==2:
-	target = socket.gethostbyname(sys.argv[1])	#Translates Hostname to IPv4
-else:
-	print("[-] Invalid Arguments !")
-	print("Syntax	: python3 scanner.py <ip>")
-	print("Example	: python3 scanner.py 127.0.0.1")
-
-#Banner
-print("-"*60)
-print("\t\tWelcome to V Port Scanner\n")
-print("\tScanning Target	: "+target)
-print("\tTime Started at	: "+str(datetime.now()))
-print("-"*60)
-
 try:
+	#Defining Target
+	if len(sys.argv)==2:
+		target = socket.gethostbyname(sys.argv[1])	#Translates Hostname to IPv4
+	else:
+		print("[-] Invalid Arguments !")
+		print("Syntax	: python3 scanner.py <ip>")
+		print("Example	: python3 scanner.py 127.0.0.1")
+		sys.exit()
+
+	#Banner
+	print("-"*60)
+	print("\t\tWelcome to V Port Scanner\n")
+	print("\tScanning Target	: "+target)
+	print("\tTime Started at	: "+str(datetime.now()))
+	print("-"*60)
+	
 	for port in range(1,65535):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
@@ -34,6 +35,8 @@ except KeyboardInterrupt:
 	
 except socket.gaierror:
 	print("[-] Hostname can't be resovled.")
+	print("Example: python3 scanner.py 127.0.0.1")
+	print("	 python3 scanner.py <domain_name>")
 	sys.exit()
 
 except socket.error:
